f8d304135767f02ddc92a73ca7bb4ff192e5c740
Maksim Bannikov <spystieber@gmail.com>
2021-02-11 12:57:55 +0300

    add publishing a building artifact to GitHub Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 val kotlinVersion by extra { "1.4.20" }
 
 plugins {
     kotlin("jvm") version "1.4.20"
+    id("maven-publish")
 }
 
 allprojects {
@@ -18,7 +20,7 @@ subprojects {
     version = "1.0.0"
 
     apply<KotlinPlatformJvmPlugin>()
-    apply(plugin = "org.gradle.maven")
+    apply(plugin = "maven-publish")
 
     dependencies {
         api(kotlin("stdlib-jdk8", kotlinVersion))
@@ -32,6 +34,25 @@ subprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("myLibrary") {
+                from(components["java"])
+            }
+        }
+
+        repositories {
+            maven {
+                name = "mescofe"
+                url = URI("https://maven.pkg.github.com/mbannikov/mescofe")
+                credentials {
+                    username = System.getenv("MAVEN_REGISTRY_USERNAME")
+                    password = System.getenv("MAVEN_REGISTRY_PASSWORD")
+                }
+            }
         }
     }
 }
